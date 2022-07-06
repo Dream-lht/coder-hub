707ee5d59ec183d0598021e83b428e711dc59897
lihuitao <lht13970619617@163.com>
2022-07-06 18:01:59 +0800

    feat:添加用户名验证和密码加密

--- service/user.service.js
+++ service/user.service.js
@@ -1,21 +1,15 @@
-const connection = require('../app/database');
+const servicePromise = require("../utils/servicePromise");
 
 class UserService{
     async createUser(user){
         const {name,password} = user;
-        console.log(name,password);
-        return new Promise((resolve,reject) => {
-            const insertUser = 'INSERT INTO `user` (name,password) VALUE (?,?);'
-            connection.execute(insertUser,[name,password],(err, results, fields) => {
-                if(err){
-                    console.log('插入失败',err);
-                    reject(err);
-                }else{
-                    console.log("插入成功",results);
-                    resolve(results);
-                }
-            })
-        })
+        const insertUser = 'INSERT INTO `user` (name,password) VALUE (?,?);'
+        return servicePromise(insertUser,[name,password]);
+    }
+
+    async getUserInfoByName(name){
+        const selectUserByName = 'select * from `user` where name=(?);'
+        return servicePromise(selectUserByName,[name]);
     }
 }
 
